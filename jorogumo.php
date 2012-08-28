4725fa55977f72a860229459f3e721be4a2e6036
Eric Sipple <saalon@gmail.com>
2012-08-28 10:24:25 -0400

    adding initial files

---
+++ jorogumo.php
@@ -0,0 +1,37 @@
+<?php
+/*
+Plugin Name: Jorogumo
+Plugin URI: http://saalonmuyo.com
+Description: Tools to keep readers on your site
+Version: 0.1
+Author: Eric Sipple
+Author URI: http://saalonmuyo.com
+License: GPL2
+*/
+
+/*  Copyright 2011  Eric Sipple  (email : saalon@gmail.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Genal Public License for more details.
+
+        You shoulerd have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+new Jorogumo();
+
+class Jorogumo {
+
+  // set up the initialization hooks for the plugin
+  function __construct() {
+
+  }
+
+}
